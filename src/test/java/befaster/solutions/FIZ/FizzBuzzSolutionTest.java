a3eea2e9a965a54088f5065731b91d004ea1e740
sbx_user1060 <sbx_user1060@ip-10-129-206-172.eu-west-2.compute.internal>
2018-09-24 21:57:51 +0000

    Mon Sep 24 21:57:51 UTC 2018

--- src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -1,5 +1,45 @@
 package befaster.solutions.FIZ;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runners.Parameterized.Parameters;
+
 public class FizzBuzzSolutionTest {
+	
+
+	private FizzBuzzSolution testSubject;
+
+	private int parameter;
+	private int expectedValue;
+
+	@Parameters(name = "{index}: {0} should return {1}")
+	public static Collection<Object[]> data() {
+		return Arrays.asList(new Object[][] { //
+				{ 10, 11 }, //
+		});
+	}
+
+	public FizzBuzzSolutionTest(final int parameter, final int expectedValue) {
+		this.parameter = parameter;
+		this.expectedValue = expectedValue;
+	}
+
+	@Before
+	public void setUp() {
+		testSubject = new FizzBuzzSolution();
+	}
+
+	@Test
+	public void compute_sum() {
+		
+		assertThat(testSubject.fizzBuzz(this.parameter), equalTo(this.expectedValue));
+	}
+
 
 }
