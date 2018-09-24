3e83a98285e74b793ff9ed80e78b983400d6eea2
sbx_user1060 <sbx_user1060@ip-10-129-206-172.eu-west-2.compute.internal>
2018-09-24 21:25:48 +0000

    Mon Sep 24 21:25:48 UTC 2018

---
+++ src/main/java/befaster/runner/BadParametersException.java
@@ -0,0 +1,11 @@
+package befaster.runner;
+
+public class BadParametersException extends RuntimeException {
+	
+	private static final long serialVersionUID = -787418499624674745L;
+
+	public BadParametersException(String message) {
+		super(message);
+	}
+
+}
